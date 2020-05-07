587af9a66a9feaa3cf054e9dd45d110c3ed1401d
Percy-chen <percy_c@163.com>
2020-05-07 01:40:50 +0000

    first commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("DEMO.GITDEMO.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
